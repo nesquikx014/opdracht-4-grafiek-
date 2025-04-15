4f5916472e64c7c8be293e03120587759b50892e
nesquikx014 <n.eroglux014@gmail.com>
2025-04-15 21:44:11 +0200

    14

--- js/main.js
+++ js/main.js
@@ -24,26 +24,20 @@ const config = {
     data: data,
   };
 
-const config2 = {
-    type: 'bar',
-    data:data,
-}
-
 new Chart(document.getElementById("js--chart--1"), config);
-new Chart(document.getElementById("js--chart--2"), config2);
 
 
 
   function getPokemon(){
 
-    let normal = 0, fighting = 0, flying = 0, poison = 0, ground = 0, rock = 0, bug = 0,
+ let normal = 0, fighting = 0, flying = 0, poison = 0, ground = 0, rock = 0, bug = 0,
  ghost = 0, steel = 0, fire = 0, water = 0, grass = 0, electric = 0, psychic = 0,
-  ice = 0, dragon = 0, dark = 0, fairy = 0, unknown = 0, shadow = 0;
+ ice = 0, dragon = 0, dark = 0, fairy = 0, unknown = 0, shadow = 0;
 
-  const labels = ['normal', 'fighting', 'poison', 'ground', 'rock', 'bug','ghost', 'steal', 'fire','water', 'water', "grass", 'electric', 'psychic', 'ice', 'dragon','dark', 'fairy','unkown', 'shadow'];
+  const labels = ['normal', 'fighting', 'poison', 'ground', 'rock', 'bug','ghost', 'steel', 'fire', 'water', 'grass', 'electric', 'psychic', 'ice', 'dragon','dark', 'fairy','unknown', 'shadow'];
 
 
-    for (let i = 0; i < 10; i++){
+    for (i = 0; i < 100; i++){
         let random = Math.floor(Math.random() * 500 +1);
         let pokemon = fetch("https://pokeapi.co/api/v2/pokemon/" + random)
                     .then(function (response){
@@ -115,8 +109,31 @@ new Chart(document.getElementById("js--chart--2"), config2);
                                                                                 
                         }
                         
-                    })
+                    }).then(function(){
+                        dataPokemon.datasets[0].data = [normal, fighting, poison, ground, rock, bug,ghost, steel, fire, water, grass, electric, psychic, ice, dragon,dark, fairy,unknown, shadow];
+                        graph.update();
+                    });
                 }
-  }
+  
+
+  const dataPokemon = {
+    labels: labels,
+
+    datasets: [
+        {
+            label: "Pokemon types",
+            data: [normal, fighting, poison, ground, rock, bug,ghost, steel, fire, water, grass, electric, psychic, ice, dragon,dark, fairy,unknown, shadow],
+            backgroundColor: ['#8F87F1', '#C68EFD','#E9A5F1', '#FED2E2' ],
+        }
+    ]
+   }
+   const configPokemon = {
+    type: 'bar',
+    data: dataPokemon,
+}
+
+const graph= new Chart(document.getElementById("js--chart--2"), configPokemon);
+
+}
 
   getPokemon();
